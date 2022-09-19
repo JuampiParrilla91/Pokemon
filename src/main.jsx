c9bc5ff61165a5df1e69be072fe2fffbe0cecb4b
Juampi Parrilla <jparrilla@culturait.com.ar>
2022-09-19 19:22:30 -0300

    Creado de carpetas,Carrusel. Instalacion de los paquetes Router y Transition

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import NavBar from "./components/layout/navBar";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <NavBar />
   </React.StrictMode>
 )
